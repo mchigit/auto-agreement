83bca7bba7b892ee84799b7d5ab0d82f1ac69607
Michael Chi <michael.chi1997@gmail.com>
2023-10-30 17:55:32 -0400

    update header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -69,7 +69,7 @@ export function StickyNavbar() {
         <div className="flex items-center justify-between text-blue-gray-900">
           <Typography
             as="a"
-            href="#"
+            href="/"
             className="mr-4 cursor-pointer py-1.5 font-medium"
           >
             Rental Lease Agreements
